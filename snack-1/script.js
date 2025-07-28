7a371c82899bf51e21c857aef4967710fc9c4273
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-07-28 16:11:13 +0200

    es1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,28 +1,7 @@
-// Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
+const hamburger = { name: "Cheese Burger", weight: 250 };
+const secondBurger = hamburger;
+secondBurger.name = "Double Cheese Burger";
+secondBurger.weight = 500;
 
-function somma(numero1, numero2) {
-  return numero1 + numero2;
-}
-console.log(somma(5, 8));
-
-// Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
-
-function sommaAnom(numero1) {
-  return function (nuemro2) {
-    return numero1 + nuemro2;
-  };
-}
-
-const Add2 = sommaAnom(2);
-const Add4 = sommaAnom(4);
-
-console.log(Add2(5));
-console.log(Add4(5));
-
-// Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.
-
-const sommaArrow = (numero1, numero2) => {
-  return numero1 + numero2;
-};
-
-console.log(sommaArrow(5, 10));
+console.log(hamburger.name); // 'Double Cheese Burger'
+console.log(secondBurger.name); // 'Double Cheese Burger'
--- snack-3/script.js
+++
@@ -1,5 +0,0 @@
-const dividi = (a, b) => a / b;
-
-const eseguiOperazione = (a, b, operazione) => operazione(a, b);
-
-console.log(eseguiOperazione(10, 2, dividi));
