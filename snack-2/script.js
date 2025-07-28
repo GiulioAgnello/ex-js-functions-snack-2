dc2843a18feb182a56ed66590868fc0eee8045b4
Giulio <agnellogiulio4@gmail.com>
2025-07-28 12:58:13 +0200

    Initial commit

---
+++ snack-2/script.js
@@ -0,0 +1,13 @@
+// Crea una arrow function che calcola il quadrato di un numero.
+
+const arrowSquare = (numero) => numero * numero;
+
+console.log(arrowSquare(5));
+
+// Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+
+function quadrato(numero) {
+  return numero * numero;
+}
+
+console.log(quadrato(8));
